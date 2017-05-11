7c9966c2e0af6cb777b0ca761b4cffcb0295ef0f
Evan Potter <epotter12@gmail.com>
2017-05-11 00:02:19 -0400

    made it so the bass-bot scrapes messages for 0 and posts the song

--- bass-bot.py
+++ bass-bot.py
@@ -1,6 +1,7 @@
 import os
 import time
 from slackclient import SlackClient
+import spotipy
 
 
 # bass-bot's ID as an environment variable
@@ -12,13 +13,14 @@ BASS_COMMAND = "drop the bass"
 
 # instantiate Slack
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+spotify = spotipy.Spotify()
 
 
-def handle_command(command, spotify_channel):
+def handle_command(command, slack_channel):
     response = "Not sure what you mean. Use the *" + BASS_COMMAND
     if command.startswith(BASS_COMMAND):
         response = "https://open.spotify.com/track/67nDjmo4SYiaVgWvFavDCb"
-    slack_client.api_call("chat.postMessage", channel=spotify_channel, text=response, as_user=True)
+    slack_client.api_call("chat.postMessage", channel=slack_channel, text=response, as_user=True)
 
 
 def parse_slack_output(slack_rtm_output):
@@ -28,6 +30,16 @@ def parse_slack_output(slack_rtm_output):
             if output and 'text' in output:
                 return output['text'], output['channel']
     return None, None
+
+
+def find_music(message_text, slack_channel):
+    text_detail = message_text.split('$[')[1].strip().lower()
+    song_name = text_detail.split('-')[0]
+    artist_name = text_detail.split('-')[1].split(']')[0]
+    q = 'artist:' + artist_name + ' track:' + song_name
+    result = spotify.search(q, 1, 0, 'track')
+    response = result['tracks']['items'][0]['external_urls']['spotify']
+    slack_client.api_call("chat.postMessage", channel=slack_channel, text=response, as_user=True)
 
 
 if __name__ == "__main__":
@@ -40,6 +52,8 @@ if __name__ == "__main__":
                 if AT_BOT in text:
                     command_text = text.split(AT_BOT)[1].strip().lower()
                     handle_command(command_text, channel)
+                if '$[' in text:
+                    find_music(text, channel)
             time.sleep(READ_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
